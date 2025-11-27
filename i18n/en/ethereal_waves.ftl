487540bfbe536a5caa52e57ed1ee16c7476cfa60
Natalie Rockot <natalierockot@gmail.com>
2025-11-27 03:27:08 -0500

    Initial code commit

---
+++ i18n/en/ethereal_waves.ftl
@@ -0,0 +1,16 @@
+app-title = Ethereal Waves
+about = About
+repository = Repository
+view = View
+welcome = Welcome to COSMIC! ✨
+page-id = Page { $num }
+git-description = Git commit {$hash} on {$date}
+about-menu = About Ethereal Waves...
+settings-menu = Settings...
+settings = Settings
+appearance = Appearance
+theme = Theme
+
+match-desktop = Match Desktop
+dark = Dark
+light = Light
---
+++ src/main.rs
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: GPL-3.0
+
+mod app;
+mod config;
+mod i18n;
+
+use app::Flags;
+use config::Config;
+use cosmic::{app::Settings, iced::Limits};
+
+fn main() -> cosmic::iced::Result {
+    // Get the system's preferred languages.
+    let requested_languages = i18n_embed::DesktopLanguageRequester::requested_languages();
+
+    // Enable localizations to be applied.
+    i18n::init(&requested_languages);
+
+    let (config_handler, config) = Config::load();
+
+    // Settings for configuring the application window and iced runtime.
+    let mut settings: Settings = Settings::default();
+    settings = settings.size_limits(Limits::NONE.min_width(360.0).min_height(180.0));
+    settings = settings.theme(config.app_theme.theme());
+
+    let flags = Flags { config_handler };
+
+    // Starts the application's event loop with `()` as the application's flags.
+    cosmic::app::run::<app::AppModel>(settings, flags)
+}
